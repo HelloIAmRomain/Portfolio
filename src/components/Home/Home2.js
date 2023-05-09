95d8aa9dfabfe7b543f708cd2fbe7ffe31e5905b
Romain DODET <romain.dodet@open-groupe.com>
2023-05-09 16:15:44 +0200

    Create a FireBase application
    Signed-off-by: Romain DODET <romain.dodet@open-groupe.com>

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -18,39 +18,84 @@ function Home2() {
             <h1 style={{ fontSize: "2.6em" }}>
               LET ME <span className="highlight"> INTRODUCE </span> MYSELF
             </h1>
+
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
-              <br />
-              <br />I am fluent in classics like
-              <i>
-                <b className="highlight"> C++, Javascript and Python. </b>
-              </i>
-              <br />
-              <br />
-              My field of Interest's are building new &nbsp;
-              <i>
-                <b className="highlight">Web Technologies and Products </b> and
-                also in areas related to{" "}
-                <b className="highlight">
-                  Deep Learning and Natural Launguage Processing.
-                </b>
-              </i>
-              <br />
-              <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="highlight">Node.js</b> and
-              <i>
-                <b className="highlight">
-                  {" "}
-                  Modern Javascript Library and Frameworks
-                </b>
-              </i>
-              &nbsp; like
-              <i>
-                <b className="highlight"> React.js and Next.js</b>
-              </i>
+                Welcome to my corner of the web!
+                I'm a self-taught tech enthusiast, with a passion for programming and
+                <i>
+                    <b className="highlight"> Artificial Intelligence</b>
+                </i>.
+                <br />
+                My favorite language?
+                <i>
+                    <b className="highlight"> Python</b> 🐍
+                </i>
+
+                <br />
+                <br />
+
+                My journey into the tech world comes from a personal interest.
+                How I learnt comes mainly from what I created.
+                I learnt through a combination of do-it-yourself projects,
+                exhaustive blog reading, and countless hours of YouTube video tutorials. 🚀
+
+                <br />
+                <br />
+
+                For instance, when it comes to crafting this website, I have faced a new challenge:
+                <i>
+                    <b className="highlight"> JavaScript</b>
+                </i>.
+                <br />
+                It was a language I wasn't particularly familiar with.
+                However, my approach to learning has always been straightforward - dive in and learn along the way when facing challenges 🌊🤿.
+                Otherwise, I would probably never have started the project if I tried to learn all prior.
+                So that's what I did. I read, I practiced, I failed, and I learned.. 👨‍💻
+                This website development comes along my personal learning of web development 🌱
+
+                <br />
+                <br />
+
+                Here is mostly where I learn to code: Mainly in&nbsp;
+                <a href="https://www.freecodecamp.org/">this website</a>
+                &nbsp;(it is free, but you may contribute to help the foundation)
+
+                <br />
+                <br />
+
+                As a&nbsp;
+                <i>
+                    <b className="highlight">Data Scientist</b> and <b className="highlight">AI practitioner</b>,
+                </i>
+                &nbsp;My experience ranges from building regression models for stock risk analysis and variance prediction in Finance,
+                to designing physics-informed neural networks simulating electromechanical elements in Robotics.
+                I've also created clustering algorithms for localization and crafted autoencoders capable of detecting fraudulent emails and corrupted PDFs in Cybersecurity. 🛡️
+
+                <br />
+                <br />
+
+                From sentiment analysis on Twitter data to designing a movie recommendation system,
+                I like to mix technology in real-world applications. 🎥🍿
+
+                <br />
+                <br />
+
+                I believe that in the world of technology, learning is constant.
+                The exciting thing is that there is an infinite amount of knowledge to explore! 📚🚀
+
+                <br />
+                <br />
+
+                Enjoy the visit, and have fun! 😄🎉
+
+                <br />
+                <br />
+
+                Let's get started! 🚀
             </p>
+
+
+
           </Col>
           <Col md={4} className="myAvtar">
             <Tilt>
