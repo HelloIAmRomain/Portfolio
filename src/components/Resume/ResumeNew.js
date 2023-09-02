b9272dfada63f0c61fc718662f3136854a80c84e
Romain Dodet <romain.dodet00@gmail.com>
2023-09-02 15:42:40 +0200

    Fix CV - Size of rendering
    Signed-off-by: Romain Dodet <romain.dodet00@gmail.com>

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -63,7 +63,7 @@ function ResumeNew() {
             variant="primary"
             href={pdf}
             target="_blank"
-            style={{ maxWidth: "250px" }}
+            style={{ maxWidth: "55px" }}
           >
             <AiOutlineDownload />
             &nbsp;Download CV
